aafcb4be72d2d9f0c492963031f0c5061e47e09c
Nori Hamamoto <nhamamoto@bandainamcoent.com>
2021-09-17 20:30:12 -0700

    (╯°□°）╯︵ ʇᴉɯɯoɔ ʇᴉuᴉ

---
+++ content.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+  const body = document.getElementsByTagName("body")[0];
+  const amount = 300;
+
+  const no_chatter =  function(f){
+    let active = false;
+
+    return (e)=>{
+      if(!active){
+        active = true;
+        f(e);
+
+      setTimeout(()=>{
+        active = false;
+      }, 150);
+      }
+    };
+  };
+
+  const down = function(e){
+    if (e.ctrlKey  && e.keyCode == 78 ){
+      scrollBy({top: amount, behavior: "smooth"});
+    }
+  };
+
+  const up = function(e){
+    if (e.ctrlKey  && e.keyCode == 80 ){
+      scrollBy({top: -amount, behavior: "smooth"});
+    }
+  };
+
+  // scroll down
+  body.addEventListener("keydown", no_chatter(down), false);
+
+  // scroll up
+  body.addEventListener("keydown", no_chatter(up), false);
+};
