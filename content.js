a10f47847bc6d6bda6c1bff7d6348dba463b8daf
Noriaki Hamamoto <norisuke3@gmail.com>
2025-03-21 15:19:19 -0700

    C-v, M-v で倍量それぞれ上下スクロールする機能を追加

--- content.js
+++ content.js
@@ -15,27 +15,29 @@ window.onload = function() {
     };
   };
 
-  const down = function(e) {
-    if (e.ctrlKey && e.keyCode == 78) {
-      e.preventDefault();
-      e.stopPropagation();
-      scrollBy({top: amount, behavior: "smooth"});
-    }
+  const down = function(e, multiplier = 1) {
+    e.preventDefault();
+    e.stopPropagation();
+    scrollBy({top: amount * multiplier, behavior: "smooth"});
   };
 
-  const up = function(e) {
-    if (e.ctrlKey && e.keyCode == 80) {
-      e.preventDefault();
-      e.stopPropagation();
-      scrollBy({top: -amount, behavior: "smooth"});
-    }
+  const up = function(e, multiplier = 1) {
+    e.preventDefault();
+    e.stopPropagation();
+    scrollBy({top: -amount * multiplier, behavior: "smooth"});
   };
 
   const handleKeyDown = function(e) {
-    if (e.ctrlKey && (e.keyCode == 78 || e.keyCode == 80)) {
+    if (e.key === 'v' && (e.ctrlKey || e.metaKey)) {
+      if (e.ctrlKey) {
+        down(e, 2);
+      } else if (e.metaKey) {
+        up(e, 2);
+      }
+    } else if (e.ctrlKey && (e.keyCode === 78 || e.keyCode === 80)) {
       e.preventDefault();
       e.stopPropagation();
-      if (e.keyCode == 78) {
+      if (e.keyCode === 78) {
         down(e);
       } else {
         up(e);
@@ -43,6 +45,5 @@ window.onload = function() {
     }
   };
 
-  // イベントリスナーを追加
   body.addEventListener("keydown", no_chatter(handleKeyDown), true);
 };
